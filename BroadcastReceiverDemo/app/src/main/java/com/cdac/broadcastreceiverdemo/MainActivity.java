bc278d7d6cf6968353de7c3537dbbf6f343b761e
sathishmepco <sathishmepco@gmail.com>
2019-05-13 15:11:11 +0530

    Changes in Broadcast Receiver App

--- BroadcastReceiverDemo/app/src/main/java/com/cdac/broadcastreceiverdemo/MainActivity.java
+++ BroadcastReceiverDemo/app/src/main/java/com/cdac/broadcastreceiverdemo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.cdac.broadcastreceiverdemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //through code register a broadcast receiver component
+        /*BroadcastReceiver receiver = new PowerBroadcastReceiver();
+        registerReceiver(receiver,new IntentFilter(Intent.ACTION_POWER_CONNECTED));
+        registerReceiver(receiver, new IntentFilter(Intent.ACTION_POWER_DISCONNECTED));*/
     }
 }
