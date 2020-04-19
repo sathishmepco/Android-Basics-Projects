a26a1d96538ad9c466d30673bb32b4ee6f6cbe05
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-04-20 00:09:43 +0530

    Mock Location Detector

--- MockLocationDetector/app/src/main/java/com/codificador/mocklocationdetector/MainActivity.java
+++ MockLocationDetector/app/src/main/java/com/codificador/mocklocationdetector/MainActivity.java
@@ -1,14 +1,66 @@
 package com.codificador.mocklocationdetector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    static final int REQUEST_CODE = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        permissionCheck();
+    }
+
+    private void permissionCheck(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE);
+            return;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getCurrentLocation();
+    }
+
+    public void getCurrentLocation() {
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    boolean isMock = location.isFromMockProvider();
+                    if(isMock){
+                        textView.setTextColor(getResources().getColor(R.color.colorAccent));
+                        textView.setText("MOCKED LOCATION");
+                    }
+                    else{
+                        textView.setTextColor(getResources().getColor(R.color.colorPrimary));
+                        textView.setText("Location from System Provider");
+                    }
+                }
+            }
+        });
     }
 }
