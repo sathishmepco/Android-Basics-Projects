db175bf9a15b87fc19e499991035988bf79da6a2
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-05-06 22:54:19 +0530

    Protect Screenshot

---
+++ ProtectScreenshot/app/src/main/java/com/codificador/protectscreenshot/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codificador.protectscreenshot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE);
+        setContentView(R.layout.activity_main);
+    }
+}
