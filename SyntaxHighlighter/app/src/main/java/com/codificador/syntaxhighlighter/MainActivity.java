0d09633f2c3a2fd45e3435e9c9ca8a5d901cee9b
sathishmepco@gmail.com <sathishmepco@gmail.com>
2020-06-23 00:12:16 +0530

    Syntax Highlighter

--- SyntaxHighlighter/app/src/main/java/com/codificador/syntaxhighlighter/MainActivity.java
+++ SyntaxHighlighter/app/src/main/java/com/codificador/syntaxhighlighter/MainActivity.java
@@ -1,14 +1,35 @@
 package com.codificador.syntaxhighlighter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        loadCode();
+    }
+
+    private void loadCode(){
+        webView.getSettings().setJavaScriptEnabled(true);
+        float density = getResources().getDisplayMetrics().density;
+        int initialScaleLevel = 100;
+        if(density == 3){
+            initialScaleLevel = 190;
+        }else if(density == 2){
+            initialScaleLevel = 130;
+        }else if(density == 4){
+            initialScaleLevel = 240;
+        }
+        webView.setInitialScale(initialScaleLevel);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.loadUrl("file:///android_asset/java_demo.html");
     }
 }
